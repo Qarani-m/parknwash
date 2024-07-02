c2d8f1156089efcddf038ceca962944236297c4e
martin <emqarani@gmail.com>
2024-07-02 23:41:44 +0300

    yet another useles commit message

--- lib/src/features/auth/screens/login.dart
+++ lib/src/features/auth/screens/login.dart
@@ -19,24 +19,27 @@ class Login extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text("Login Page"),
-                ElevatedButton(
-                    onPressed: loginController.loginEmailAndPassword,
-                    child: Text("Email Password")),
-                SizedBox(
-                  height: 50.h,
-                ),
-                ElevatedButton(
-                    onPressed: loginController.loginEmailAndPassword,
-                    child: Text("Google")),
-                SizedBox(
-                  height: 50.h,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Get.toNamed("/register");
-                    },
-                    child: Text("Register"))
+
+                
+                // Text("Login Page"),
+
+                // ElevatedButton(
+                //     onPressed: loginController.loginEmailAndPassword,
+                //     child: Text("Email Password")),
+                // SizedBox(
+                //   height: 50.h,
+                // ),
+                // ElevatedButton(
+                //     onPressed: loginController.loginEmailAndPassword,
+                //     child: Text("Google")),
+                // SizedBox(
+                //   height: 50.h,
+                // ),
+                // ElevatedButton(
+                //     onPressed: () {
+                //       Get.toNamed("/register");
+                //     },
+                //     child: Text("Register"))
               ],
             ),
           ),
