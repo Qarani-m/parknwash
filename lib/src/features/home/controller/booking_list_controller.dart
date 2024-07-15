9bc1c73279adbf4fa705bdf362f6c37dd8c86338
martin <emqarani@gmail.com>
2024-07-16 01:02:39 +0300

    .

--- lib/src/features/home/controller/booking_list_controller.dart
+++ lib/src/features/home/controller/booking_list_controller.dart
@@ -1,5 +1,69 @@
-import 'package:get/get.dart';
+import 'dart:convert';
 
-class BookingListController extends GetxController{
-  
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:parknwash/src/features/home/models/booking_model.dart';
+import 'package:parknwash/src/utils/constants/firebase_service.dart';
+
+class BookingListController extends GetxController {
+  RxList<BookingData> bookings = <BookingData>[].obs;
+  final box = GetStorage();
+
+  @override
+  Future<void> onInit() async {
+    // TODO: implement onInit
+    super.onInit();
+    // await getBookings();
+
+    // getBookings(await extractUid());
+    getBookings("pSgDcrX5XtaXujizeObCw3o5CWb2");
+  }
+
+  Future<String> extractUid() async {
+    String jsonString = box.read("userData");
+    Map<String, dynamic> userData = jsonDecode(jsonString);
+    String uid = userData['uid'];
+    return uid;
+  }
+
+  Future<void> getBookings(String targetUid) async {
+    try {
+      final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('bookings')
+          .where('userId', isEqualTo: targetUid)
+          .get();
+
+      List<BookingData> theData = <BookingData>[];
+
+      if (querySnapshot.docs.isNotEmpty) {
+        for (var doc in querySnapshot.docs) {
+          Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+          theData.add(BookingData(
+              eta: data["eta"]?? 0,
+              lotId: data['lotId']?? "",
+              phone: data['phone']?? "",
+              status: data['status']?? "",
+              timestamp: data['timestamp']?? Timestamp(2,0),
+              userId: data["userId"]?? "",
+              vehicleRegNo: data['vehicleRegNo'] ?? "",
+              name: data["name"]??""
+              
+              ));
+
+        }
+        print(theData);
+
+        bookings.value = theData;
+      } else {
+        print('No bookings found for user: $targetUid');
+      }
+    } catch (e) {
+      print('Error fetching booking: $e');
+
+      Get.snackbar("Error", "Some tyoe of error of error happened ${e}");
+    }
+  }
 }
---
+++ lib/src/utils/constants/firebase_service.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService<T> {
+  final String collection;
+  final T Function(Map<String, dynamic>) fromJson;
+  final Map<String, dynamic> Function(T) toJson;
+
+  FirestoreService({
+    required this.collection,
+    required this.fromJson,
+    required this.toJson,
+  });
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Create
+  Future<String> addDocument(T data) async {
+    try {
+      DocumentReference docRef = await _firestore.collection(collection).add(toJson(data));
+      return docRef.id;
+    } catch (e) {
+      print('Error adding document: $e');
+      rethrow;
+    }
+  }
+
+  // Read
+  Future<List<T>> getDocuments() async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore.collection(collection).get();
+      return querySnapshot.docs.map((doc) => fromJson(doc.data() as Map<String, dynamic>)).toList();
+    } catch (e) {
+      print('Error getting documents: $e');
+      rethrow;
+    }
+  }
+
+  Future<T?> getDocument(String documentId) async {
+    try {
+      DocumentSnapshot docSnapshot = await _firestore.collection(collection).doc(documentId).get();
+      if (docSnapshot.exists) {
+        return fromJson(docSnapshot.data() as Map<String, dynamic>);
+      }
+      return null;
+    } catch (e) {
+      print('Error getting document: $e');
+      rethrow;
+    }
+  }
+
+  // Update
+  Future<void> updateDocument(String documentId, T data) async {
+    try {
+      await _firestore.collection(collection).doc(documentId).update(toJson(data));
+    } catch (e) {
+      print('Error updating document: $e');
+      rethrow;
+    }
+  }
+
+  // Delete
+  Future<void> deleteDocument(String documentId) async {
+    try {
+      await _firestore.collection(collection).doc(documentId).delete();
+    } catch (e) {
+      print('Error deleting document: $e');
+      rethrow;
+    }
+  }
+
+  // Stream of documents
+  Stream<List<T>> streamDocuments() {
+    return _firestore.collection(collection).snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => fromJson(doc.data() as Map<String, dynamic>)).toList();
+    });
+  }
+}
